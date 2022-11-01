7fd2a3386a0e31a246fd7980cdf0338e35ea6a84
jestemboski999 <jestemboski999@gmail.com>
2022-11-01 20:30:20 +0100

    FW v1

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,12 +1,7 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
     public:
-    Wektor2D()
-    {
-        setX(0.0);
-        setY(0.0);
-    };
+    Wektor2D(){};
     Wektor2D(double in_x, double in_y) //Konstruktor
     {
         
@@ -18,9 +13,10 @@ class Wektor2D
     double getX() {return x;}
     void setY(double w_y) {y = w_y;}
     double getY() {return y;}
+    
 
     private:
-    int x,y;
+    double x,y;
 };
 
 Wektor2D operator+(Wektor2D w1, Wektor2D w2)
@@ -33,12 +29,12 @@ Wektor2D operator+(Wektor2D w1, Wektor2D w2)
     return Wektor2D{x1+x2, y1+y2};
 }
 
-Wektor2D operator*(Wektor2D w1, Wektor2D w2)
+double operator*(Wektor2D w1, Wektor2D w2)
 {
     double x1,y1,x2,y2;
     x1= w1.getX();
     x2= w2.getX();
     y1= w1.getY();
     y2= w2.getY();
-    return Wektor2D{x1*x2, y1*y2};
+    return (x1*x2)+(y1*y2);
 }
